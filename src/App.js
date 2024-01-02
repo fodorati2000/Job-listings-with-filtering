f84ab8c08f8f226e975258687f3ba8119138951b
fodorati2000 <32171508+fodorati2000@users.noreply.github.com>
2024-01-02 20:32:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,150 @@
-import logo from './logo.svg';
+
+import { useEffect, useState, useMemo } from 'react';
+
 import './App.css';
 
+import { array } from './list';
+import searcDelete from './img/icon-remove.svg';
+
+function Job({ avatar, name, status, featured, title, time, workType, localType, tag, ontagClick }) {
+
+
+  return (
+    <div className="jobList">
+      <img className="logo" src={avatar} alt="company logo" />
+      <div className="sectionTitle">
+        <div className="info">
+          <div className="name">{name}</div>
+          <div className={status ? 'status' : ''}> {status ? 'NEW!' : ''}</div>
+          <div className={featured ? 'featured' : ''}> {featured ? 'FEATURED' : ''}</div>
+        </div>
+
+        <div className="title" >{title}</div>
+
+        <div className="bottominfo">
+          <div className="time">{time}</div>
+          <div className="worktime">{workType}</div>
+          <div className="local">{localType}</div>
+        </div>
+      </div>
+      <div className="skill">
+        {tag.map((i) => {
+          return (
+            <div className="tag" onClick={() => ontagClick(i)} key={i}>
+              {i}
+            </div>
+          );
+        }
+
+        )}
+      </div>
+    </div>
+  );
+
+};
+
+
 function App() {
+  const [search, setSearch] = useState([]);
+  const [list, setList] = useState(array);
+  const [list2, setList2] = useState([]);
+  
+  //this function create one array and this array have all search tag
+  function searchTag(i) {
+    let a = false;
+    if (search.length === 0) {
+   
+      setSearch([...search, i]);
+
+    } else {
+     
+      search.map((tag) => {
+        if (tag === i) { a = true;}
+      });
+      if (!a) { setSearch([...search, i]); } else { a = false; }
+    }
+  }
+
+  useEffect(() => {
+    let put = true;
+    let result = [];
+
+    list.map((element) => {
+      search.map((s) => {
+        if (element.tag.includes(s)) {
+          if (put) {
+            put = true;
+          }
+        } else {
+          put = false;
+
+        }
+      })
+      if (put) {
+        result = [...result, element];
+      } else {
+        put = true;
+      }
+    })
+    setList2(result);
+
+  }, [search]);
+
+
+  const deleteByValue = value => {
+    setSearch(oldValues => {
+      return oldValues.filter(item => item !== value)
+    })}
+
+    
+
+  let visible = search.length > 0 ? "visible" : "hiding";
+  let listChanger = list2.length > 0 ? list2 : list;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className={visible}>
+          {
+        
+              search.map((y)=>{
+                
+                return(
+                  <div className="items" key={y}>
+                    {y}
+                    <button onClick={() => {deleteByValue(y)}} className='deletebutton'><img src={searcDelete}/></button>
+                  </div>
+                )
+                
+              })                              
+          }
+        </div>
+      </div>
+      <div className="main" >
+
+        {listChanger.map((row) => {
+
+          return (
+            <Job
+              key={row.id}
+              avatar={row.avatar}
+              name={row.name}
+              status={row.status}
+              featured={row.featured}
+              title={row.title}
+              time={row.time}
+              workType={row.workType}
+              localType={row.localType}
+              tag={row.tag}
+              ontagClick={searchTag}
+            />)
+        })}
+
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/list.js
@@ -0,0 +1,26 @@
+
+
+import photosnap from "./img/photosnap.svg";
+import manage from "./img/manage.svg";
+import account from "./img/account.svg";
+import MyHome from "./img/myhome.svg";
+import LoopStudios from "./img/loop-studios.svg";
+import FaceIt from "./img/faceit.svg";
+import Shortly from "./img/shortly.svg";
+import Insure from "./img/insure.svg";
+import EyecamCo from "./img/eyecam-co.svg";
+import AirFilterCompany from "./img/the-air-filter-company.svg";
+
+
+export const array = [
+    {"id": 0, "avatar": photosnap, "name": "Photosnap", "status":true, "featured": true, "title": "Senior Frontend Developer", "tag":["Frontend","Senior", "HTML", "CSS", "Javascript"],"time":"1d ago", "workType":"Full Time","localType":"USA only"},
+    {"id": 1, "avatar": manage, "name": "Manage", "status": true, "featured": true, "title": "Fullstack Developer", "tag":["Fullstack","Midweight", "Python", "React"],"time":"1d ago", "workType":"Part Time","localType":"USA only"},
+    {"id": 2, "avatar": account, "name": "Account", "status": true, "featured":false, "title": "Junior Frontend Developer", "tag":["Frontend","Junior", "React", "Sass", "Javascript"],"time":"2d ago", "workType":"Part Time","localType":"USA only"},
+    {"id": 3, "avatar": MyHome, "name": "MyHome", "status": false, "featured":false, "title": "Junior Frontend Developer", "tag":["Frontend","Junior", "CSS", "Javascript"],"time":"5d ago", "workType":"Full Time","Contract":"USA only"},
+    {"id": 4, "avatar": LoopStudios, "name": "Loop Studios", "status": false, "featured": false, "title": "Software Enginer", "tag":["Fullstack","Midweight", "Javascript", "Sass", "Ruby"],"time":"1w ago", "workType":"Full Time","localType":"Wordwide"},
+    {"id": 5, "avatar": FaceIt, "name": "FaceIt", "status": false, "featured": false, "title": "Junior Frontend Developer", "tag":["Backend","Junior", "Ruby", "RoR"],"time":"2w ago", "workType":"Full Time","localType":"UK only"},
+    {"id": 6, "avatar": Shortly, "name": "Shortly", "status": false, "featured": false, "title": "Junior Developer", "tag":["Frontend","Junior", "HTML", "Saas", "Javascript"],"time":"2w ago", "workType":"Full Time","localType":"Worldwide"},
+    {"id": 7, "avatar": Insure, "name": "Insure", "status": false, "featured": false, "title": "Junior Frontend Developer", "tag":["Frontend","Junior", "Vue", "Javascript", "Sass"],"time":"2w ago", "workType":"Full Time","localType":"USA only"},
+    {"id": 8, "avatar": EyecamCo, "name": "Eyecam Co.", "status": false, "featured": false, "title": "Full Stack Enginer", "tag":["Fullstack","Midweight", "Javascript", "Django", "Python"],"time":"3w ago", "workType":"Full Time","localType":"Worldwide"},
+    {"id": 9, "avatar": AirFilterCompany, "name": "The Air Filter Company", "status": false, "featured": false, "title": "Front-end Dev", "tag":["Frontend","Junior", "React", "Sass", "Javascript"],"time":"1m ago", "workType":"Part Time","localType":"Worldwide"},
+]
